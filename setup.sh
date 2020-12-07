98efd2c340fd199012fcf51d2fac9bf7e7b91888
a78275 <a78275@alunos.uminho.pt>
2020-12-07 16:44:02 +0000

    setup, requirements and tests

--- setup.sh
+++ setup.sh
@@ -5,6 +5,11 @@ LIST_OF_APPS="iputils-ping software-properties-common nmap wireshark"
 
 sudo apt update
 sudo apt install $SNMP_APPS
+sudo apt install lldpad
+lldpad -d
+
+for i in `ls /sys/class/net/ | grep 'eth\|ens\|eno'` ; do echo "enabling lldp for interface: $i" ; lldptool set-lldp -i $i adminStatus=rxtx ; lldptool -T -i $i -V sysName enableTx=yes; lldptool -T -i $i -V portDesc enableTx=yes ; lldptool -T -i $i -V sysDesc enableTx=yes; lldptool -T -i $i -V sysCap enableTx=yes; lldptool -T -i $i -V mngAddr enableTx=yes; done
+
 sudo apt install -y $PYTHON_APPS
 sudo add-apt-repository universe
 pip3 install --upgrade pip
